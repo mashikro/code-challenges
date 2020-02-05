cc0998e4b12939004f1eec19a585321e4cff9d34
Masha Ikromova <mashikro@gmail.com>
2020-02-05 13:34:33 -0500

    Added runtime to initial algo and added a faster runtime version which uses a hashmap

--- two_sum.py
+++ two_sum.py
@@ -13,7 +13,7 @@
 # return [0, 1]
 
 
-def find_two_sum2(nums, target):
+def find_two_sum(nums, target):
 
     index_1 = None
     index_2 = None
@@ -29,11 +29,32 @@ def find_two_sum2(nums, target):
             index_2 = i
 
     return [index_1, index_2]
+# runtime: O(n2)+O(n) = O(n^2)
+
+# print(find_two_sum([3,2,3], 6)) #[0,2]
+# print(find_two_sum([2, 7, 11, 15], 9)) #[0, 1]
+# print(find_two_sum([3,2,4], 6)) #[1,2]
 
 
+def find_two_sum2(nums, target):
+
+    seen = {}
+
+    for i in range(len(nums)):
+        looking_for = target - nums[i]
+
+        if looking_for in seen: 
+
+            return[seen[looking_for], i]
+
+        else:
+            seen[nums[i]] = i
+    return []
+
+#runtime is O(n)
+print(find_two_sum2([3,2,4], 6)) #[1,2]
 print(find_two_sum2([3,2,3], 6)) #[0,2]
 print(find_two_sum2([2, 7, 11, 15], 9)) #[0, 1]
-print(find_two_sum2([3,2,4], 6)) #[1,2]
 
 
 
