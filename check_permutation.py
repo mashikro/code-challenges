9b481ca5ba966f523735ed2ccc5392c6b1645fd8
Masha Ikromova <mashikro@gmail.com>
2020-02-09 10:00:56 -0500

    Added algo thats O(n) time

--- check_permutation.py
+++ check_permutation.py
@@ -28,3 +28,19 @@ print(check_permutation('apple', 'alppw')) #False
 # runtime: O(n)*O(n) -> O(n^2)
 
 
+#from solution that uses python's built in Counter()
+# O(N)
+from collections import Counter
+
+
+def check_permutation(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    counter = Counter()
+    for c in str1:
+        counter[c] += 1
+    for c in str2:
+        if counter[c] == 0:
+            return False
+        counter[c] -= 1
+    return True
