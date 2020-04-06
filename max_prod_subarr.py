e69c9dec7dcaaeac4055bcc9995899214904e472
Masha Ikromova <mashikro@gmail.com>
2020-04-06 12:32:37 -0400

    Algorithm for finding max continous prod subarr in an arr

--- max_prod_subarr.py
+++ max_prod_subarr.py
@@ -10,20 +10,29 @@
 # Output: 2
 
 def find_max_product(nums):
-
-    if len(nums) < 2:
+    if len(nums) == 0:
+        return 0
+    if len(nums) <2:
         return nums[0]
+    
+    current_max = nums[0]
+    current_min = nums[0]
+    final_max = nums[0]
 
-    current = nums[0]
-    max_ = nums[0]
+    for i in range(1, len(nums)):
 
+        temp_current_max = current_max
 
-    for num in nums[1:]:
+        current_max = max(max(current_max*nums[i], current_min*nums[i]), nums[i])
+        current_min = min(min(temp_current_max*nums[i], current_min*nums[i]), nums[i])
+        final_max = max(current_max, final_max)
 
-        current = max(num, (current * num))
-        max_ = max(max_, current)
+        print('current_max', current_max)
+        print('current_min', current_min)
+        print('FINAL', final_max)
 
-    return max_
+    return final_max
+
 
 print(find_max_product([2,3,-2,4])) #6
 print(find_max_product([2])) #2
@@ -31,3 +40,40 @@ print(find_max_product([2])) #2
 print(find_max_product([-2,3,-4])) #24
 
 print(find_max_product([-4,-3])) #12
+
+# Runtime: O(n) time and O(1) space
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def find_max_product2(nums):
+    positiveArray = [0] * len(nums) 
+    negativeArray = [0] * len(nums)
+    if nums[0] > 0:
+        positiveArray[0] = nums[0]
+    else:
+        if len(nums) == 1:
+            return nums[0]
+        negativeArray[0] = nums[0]
+    for i in range(1,len(nums)):
+        if nums[i] < 0:
+            positiveArray[i] = negativeArray[i-1]*nums[i]
+            negativeArray[i] = min(nums[i],positiveArray[i-1]*nums[i])
+        if nums[i] > 0:
+            positiveArray[i] = max(nums[i],positiveArray[i-1]*nums[i])
+            negativeArray[i] = negativeArray[i-1]*nums[i]
+    return max(positiveArray)
