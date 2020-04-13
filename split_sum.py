8117fcc47f321fd26622e17f72cbf4603d931154
Masha Ikromova <mashikro@gmail.com>
2020-04-13 14:30:38 -0400

    Algorithm for checking if an arr can be split into 2 equal subarrs

--- split_sum.py
+++ split_sum.py
@@ -1,24 +1,69 @@
-# Prompt: From Facebook production engineer role q:split array in such a way that their sum is same 
+# Prompt: From Facebook production engineer role:
+# Given a non-empty array containing only positive integers, find if the array can be 
+# partitioned into two subsets such that the sum of elements in both subsets is equal.
 
-# Test Case: 
-# Input: [1, 5, 4, 3, 2, 5]
-# Output: 2 list that sum to 10 each
+# Note:
+# Each of the array element will not exceed 100.
+# The array size will not exceed 200.
+
+# Example 1:
+
+# Input: [1, 5, 11, 5]
+
+# Output: true
+
+# Explanation: The array can be partitioned as [1, 5, 5] and [11].
 
 def split_sum(lst):
 
-    target_sum = sum(lst)/2
-    print('target_sum',target_sum)
+    if len(lst) < 2:
+        return False
 
-    lst_1 = []
-    lst_2 = []
+    target = sum(lst)/2
+    # print('target',target)
 
-    for num in lst:
+    lst.sort(reverse=True)
+    # print('lst.sort()', lst)
 
-        if sum(lst_1) < target_sum:
-            lst_1.append(num)
-        else:
-            lst_2.append(num)
 
-    return (lst_1, lst_2)
+    sum_1 = 0
+    sum_2 = 0
 
-print(split_sum([1, 5, 4, 3, 2, 5]))
+    # print('sum_1', sum_1)
+    # print('sum_2', sum_2)
+
+    i = 0
+    while i < len(lst): 
+        looking_for_1 = target - sum_1
+        looking_for_2 = target - sum_2
+        if (sum_1 < sum_2):
+            if (sum_1 < target):
+                if looking_for_1 in lst:
+                    sum_1 += looking_for_1
+                    lst.remove(looking_for_1)
+                    i -=1 #list became shorter
+                else:
+                    sum_1+=(lst[i]) #if looking_for not in lst add next available
+        
+        else: 
+            if (sum_2 < target):
+                if looking_for_2 in lst:
+                    sum_2 += looking_for_2
+                    lst.remove(looking_for_2)
+                    i-=1
+                else:
+                    sum_2+=(lst[i])
+        i+=1
+      
+    return sum_1 == sum_2
+
+print(split_sum([3,3,3,4,5])) #True
+print(split_sum([1,2,3,4,5,6,7])) # True
+print(split_sum([1, 5, 11, 5])) #True
+print(split_sum([1])) #False
+print(split_sum([1, 2, 3, 5])) #False
+print(split_sum([[2,2,3,5]])) #False
+
+
+
+
